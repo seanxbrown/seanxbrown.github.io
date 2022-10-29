07c267d3a7e2cf30b5b9a9388d8ae55e3a618bef
Sean Brown <seanbrown82@hotmail.com>
2022-10-29 14:52:16 +0100

    Feat: Add fade-in to landing text

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-ECHO is on.
+const landingTextFadeIn = () => {
+
+    const startOfHeader = document.getElementById("startOfHeader");
+    const restOfHeader = document.getElementById("restOfHeader");
+    const landingText = document.getElementsByClassName("landingText");
+
+    for (let text of landingText) {
+        text.classList.add("revealText");
+    }
+
+    startOfHeader.classList.add("revealText");
+    restOfHeader.classList.add("revealText");
+        
+
+}
+
+landingTextFadeIn()
